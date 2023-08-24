5ff1f22cce0d5f13477963ba1941dbb75f7b765e
Gokarla Srinivas <87422030+srinivasGokarla@users.noreply.github.com>
2023-08-24 10:42:56 +0530

    Create numbercheck.js

---
+++ numbercheck.js
@@ -0,0 +1,12 @@
+//Write a function to check if a given number is even or odd
+
+let n = 13;
+function check(n) {
+   if(n % 2 == 0) {
+       console.log("Even Number")
+   } else {
+       console.log("Odd Number")
+   }
+    
+}
+check(n)
