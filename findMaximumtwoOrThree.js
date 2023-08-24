6422046692fc572df0a1bc0de693f9898f808c37
Gokarla Srinivas <87422030+srinivasGokarla@users.noreply.github.com>
2023-08-24 11:06:45 +0530

    findMaximumtwoOrThree

---
+++ findMaximumtwoOrThree.js
@@ -0,0 +1,25 @@
+//Write a function to find the maximum of two numbers.
+
+let a  = 11; let b = 11;
+
+if(a < b) {
+    console.log("b:",b)
+} else if(a > b) {
+    console.log("a:",a)
+} else {
+    console.log("Two Are Equal")
+}
+
+
+//Write a function to find the maximum of three numbers.
+let[a,b,c] = [12,17,9]
+
+if(a < b && c < b) {
+    console.log("b:",b)
+} else if(a < c && b < c) {
+    console.log("c:",c)
+} else if(a < b && c < a) {
+    console.log("a:", a)
+} else {
+    console.log("All Are Equal")
+}
