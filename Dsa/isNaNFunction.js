473fafb430006c9e742f919591b216d4350a3725
srinivasGokarla <srinivasgokarla@gmail.com>
2023-09-26 14:59:57 +0530

    tuesday

---
+++ Dsa/isNaNFunction.js
@@ -0,0 +1,12 @@
+/*
+ * Write your isNumberNaN function here
+ */
+
+function isNumberNaN(value) {
+    if(isNaN(value)) {
+        return true
+    } else {
+        return false
+    }
+}
+export {isNumberNaN}
