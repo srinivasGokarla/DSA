3cf2519258611e0724b83bfbee304c65a92e6ffc
srinivasGokarla <srinivasgokarla@gmail.com>
2023-11-22 17:29:28 +0530

    updated

--- practice/src/Component/HomePage.jsx
+++ practice/src/Component/HomePage.jsx
@@ -1,5 +1,34 @@
+import { useEffect } from "react"
+import {  useNavigate } from "react-router-dom"
+
+
 export const HomePage = () =>{
+    const navigate = useNavigate()
+
+    const ifLocalStorageExist = ()=>{
+        // console.log(localStorage.getItem('user') === null)
+        // console.log(window.location.href)
+      if( localStorage.getItem('user') !== null) {
+           navigate('/')
+           localStorage.clear()
+      }
+    }
+
+    useEffect(()=>{
+      
+        const check = () => {
+          let homeCheck = window.location.href.split("/")
+        if ( localStorage.getItem('user') === null && homeCheck[3] === "home"){
+        navigate('/')
+      } 
+
+     
+    }
+
+    check()
+    },[])
     return <div>
         <h1>welcome to home page</h1>
-    </div>
+        <button onClick={ifLocalStorageExist}>Logout</button>
+    </div>
 }
