67ffc0bfc637678667db66751adf4fb1ac81fc21
srinivasGokarla <srinivasgokarla@gmail.com>
2024-01-10 18:09:17 +0530

    Rocked problem

--- Dsa/rocked.js
+++ Dsa/rocked.js
@@ -7,41 +7,29 @@ let data = [
     { "id": 'labrador', 'parent': 'dogs' },
     { "id": 'persian', 'parent': 'cats' },
     { "id": 'siamese', 'parent': 'cats' }
-]
-function herchary(data) {
-  //  console.log(data)
-    let obj = {};
-    console.log(obj, "testing")
-    function build(parent,node) {
-     //console.log(parent,node "checking")
-        const child = data.filter((item) =>  item.parent == 'animals')
-        
-       
-  .map((element) => {
-      
-      node[element.id] = {};
-      
-      console.log(node[element.id])
-      return  build(node[element.id], element.id)
-      console.log(child)
-  })
-  
-  if( build(node, null) == null) {
-     return
-  } else {
-      build(node, null) 
-     return  obj
-  }
-     
-     
-     
-    }
-    
+];
 
+function buildHierarchy(data, parent) {
+    let result = {};
+    data
+        .filter(item => item.parent === parent)
+        .forEach(item => {
+            const children = buildHierarchy(data, item.id);
+            if (Object.keys(children).length > 0) {
+                result[item.id] = children;
+            } else {
+                result[item.id] = {};
+            }
+        });
+    return result;
 }
-console.log(herchary(data))
 
+const root = data.find(item => item.parent === null);
+const output = buildHierarchy(data, root.id);
 
+console.log(JSON.stringify(output, null, 2));
+
+//output: 
 // {
 //     "mammals": {
 //         "cats": {
