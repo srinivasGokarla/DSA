b1ef0565915de616049ff71922a9e988c2c85971
srinivasGokarla <srinivasgokarla@gmail.com>
2024-01-12 12:47:16 +0530

    interview

---
+++ Dsa/design.js
@@ -0,0 +1,18 @@
+const dataArray = [
+    { name: "John", class: "3", skill: { one: "reading", two: "math" } },
+    { name: "Alice", class: "2", skill: { one: "drawing", two: "music" } },
+    { name: "Bob", class: "6", skill: { one: "coding", two: "swimming" } },
+    { name: "Eva", class: "4", skill: { one: "writing", two: "singing" } },
+    { name: "Mike", class: "1", skill: { one: "playing", two: "cooking" } },
+    { name: "Sophie", class: "5", skill: { one: "dancing", two: "programming" } }]
+    
+    
+  // const names =  dataArray.map((item) => ({
+  //     name: item.name
+  // })
+  
+  const twoValues = dataArray.map((item) => (
+      {
+          two : item.skill.two
+      }))
+  
